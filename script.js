c0e0d2fd5cf2ef7badc4c8f90d7f8b11b31e9492
MrForestino <borv2006@gmail.com>
2024-07-01 17:00:28 +0200

    update

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const mandarin = document.getElementById('mandarin');
 mandarin.addEventListener('click', () => {
     score++;
     scoreText.textContent = score;
+
+const increment = document.createElement('div');
+increment.textContent = '+1'
+increment.classList.add('increment');
+document.body.appendChild(increment);
+
+increment.style.left = `${event.clientX}px`;
+increment.style.top = `${event.clientY}px`;
+
+increment.addEventListener('animationend', () => {
+    increment.remove();
+});
+
 });
